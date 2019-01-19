d594f7b465e06e0880358a44daff19966a455fbd
Mash Wimalawardana <mashdroidd@gmail.com>
2019-01-20 08:13:22 +1100

    Added Docs Folder for gh-pages

---
+++ docs/precache-manifest.169b966570616de7fc1d6c0dbfb82338.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9407d0ff83c95101bc93",
+    "url": "/static/js/runtime~main.9407d0ff.js"
+  },
+  {
+    "revision": "0f7d5018015b393ee54d",
+    "url": "/static/js/main.0f7d5018.chunk.js"
+  },
+  {
+    "revision": "721fb9f64f63b8394c50",
+    "url": "/static/js/3.721fb9f6.chunk.js"
+  },
+  {
+    "revision": "52792356829ed8f33b6b",
+    "url": "/static/js/1.52792356.chunk.js"
+  },
+  {
+    "revision": "a931d04b90102c205635",
+    "url": "/static/js/0.a931d04b.chunk.js"
+  },
+  {
+    "revision": "0f7d5018015b393ee54d",
+    "url": "/static/css/main.af540b02.chunk.css"
+  },
+  {
+    "revision": "721fb9f64f63b8394c50",
+    "url": "/static/css/3.14c11a17.chunk.css"
+  },
+  {
+    "revision": "a931d04b90102c205635",
+    "url": "/static/css/0.297b9c93.chunk.css"
+  },
+  {
+    "revision": "0e7c46abf3b2665650da22d16f56208e",
+    "url": "/index.html"
+  }
+];
