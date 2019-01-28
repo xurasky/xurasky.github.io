22d428647c88d6790bf38907a08fc6492f23cad4
Mash Wimalawardana <mashdroidd@gmail.com>
2019-01-28 16:37:34 +1100

    Removed Footer

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navigation from "../components/navigation";
-import Footer from "../components/footer";
 import paths from "./paths";
 
 const App = () => (
@@ -13,7 +12,6 @@ const App = () => (
           return <Route exact key={index} path={item.path} component={item.component} />;
         })}
       </Switch>
-      <Footer />
     </div>
   </BrowserRouter>
 );
